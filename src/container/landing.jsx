6f9451c9eaf7c8359c03499dd9924c617802fb81
ange-boyz <angeboyz@icloud.com>
2025-04-18 10:05:32 +0100

    start with the landing page

--- src/container/landing.jsx
+++ src/container/landing.jsx
@@ -3,8 +3,34 @@ import { NavLink } from "react-router-dom";
 
 const Landing = () =>{
     return(
-        <div className="text-3xl t">
-            <NavLink to="SignUp">My Landing page</NavLink></div>
+        <div className=" text-white bg-[#000008] h-[100vh] md:px-10">
+            <div className="flex justify-between items-center h-[70px]  px-2">
+                <div className="logo text-blue-500 font-bold text-3xl">KEMCHAT</div>
+
+                <div className="md:flex space-x-6 hidden">
+                    <button>About Us</button>
+                    <button>Contact Us</button>
+                </div>
+                <nav>   
+                    <ul className="flex space-x-4">
+                    <button className=" py-1 px-3 rounded-full ">
+                        <NavLink to="/login">Login</NavLink></button>
+                    <button className="bg-blue-600 py-1 px-3 rounded-full">
+                        <NavLink to="signUp">Sign In</NavLink></button>
+                    </ul>
+                </nav>
+            </div>
+
+            <div className="">
+                <div className="">
+                    <h1 className="text-8xl font-bold">Welcome to <span className="text-blue-600">KEMCHAT</span></h1>
+                    <p className="text-4xl">The platforme is not yet ready, but we have a static website for the moment...so feel
+                         free to discover our platform</p>
+                    <button className="bg-blue-600 py-1 px-3 rounded-full mt-5">
+                         <NavLink to="signUp">Disover -</NavLink></button>  
+                </div>
+            </div>
+        </div>
     )
 }
 
